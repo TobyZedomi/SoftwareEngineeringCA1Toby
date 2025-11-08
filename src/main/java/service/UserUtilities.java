064339eca38dbc76a2684fef387e91bd1641ed67
TobyZedomi <tobyzedo4@gmail.com>
2025-11-08 20:50:15 +0000

    Adding server methods to view all artist and also to search for artist

--- src/main/java/service/UserUtilities.java
+++ src/main/java/service/UserUtilities.java
@@ -3,7 +3,7 @@ package service;
 public class UserUtilities {
 
     public static final String HOSTNAME = "localhost";
-    public static final int PORT = 11000;
+    public static final int PORT = 12000;
 
 
     // REQUESTS
@@ -11,13 +11,19 @@ public class UserUtilities {
 
     public static final String REGISTER = "REGISTER";
 
+    public static final String SEARCH_FOR_ARTIST = "SEARCH_FOR_ARTIST";
+
+    public static final String GET_ALL_ARTIST = "GET_ALL_ARTIST";
+
     public static final String EXIT = "EXIT";
     public static final String LOGOUT = "LOGOUT";
 
 
+    //RESPONSES
 
     public static final String LOGIN_SUCCESSFUL = "LOGIN_SUCCESSFUL";
     public static final String LOGIN_FAILED = "LOGIN_FAILED";
+
 
     public static final String PASSWORDS_DONT_MATCH = "PASSWORDS_DONT_MATCH";
 
@@ -32,10 +38,28 @@ public class UserUtilities {
 
     public static final String NOT_LOGGED_IN = "NOT_LOGGED_IN";
 
+    public static final String ARTISTS_RETRIEVED_SUCCESSFULLY = "ARTISTS_RETRIEVED_SUCCESSFULLY";
+
+    public static final String YOU_HAVE_NO_ARTISTS = "YOU_HAVE_NO_ARTISTS";
+
+
+    public static final String NO_ARTISTS_WITH_THIS_NAME = "NO_ARTISTS_WITH_THIS_NAME";
+
+    public static final String EMPTY_ARTIST_NAME = "EMPTY_ARTIST_NAME";
+
+
+
+    // DELIMITERS
+    public static final String DELIMITER = "%%";
+    public static final String ARTIST_DELIMITER = ", ";
+
+    public static final String ARTIST_DELIMITER2 = "##";
+
 
     // GENERAL MALFORMED RESPONSE:
     public static final String INVALID = "INVALID";
 
+    public static final String GOODBYE = "GOODBYE";
 
 
 }
---
+++ src/main/java/sql/software_engineering_dataset.sql
@@ -0,0 +1,12 @@
+INSERT INTO `users` (`username`, `password`)
+VALUES  ('Toby', '$2a$12$al0T3VLHPQM4kiZqgyitj.rDgVDSI1xc5y2SCkbo8ZJDTJ5WOZl5a'),
+        ('Sean', '$2a$12$al0T3VLHPQM4kiZqgyitj.rDgVDSI1xc5y2SCkbo8ZJDTJ5WOZl5a'),
+        ('Sarah', '$2a$12$al0T3VLHPQM4kiZqgyitj.rDgVDSI1xc5y2SCkbo8ZJDTJ5WOZl5a');
+
+
+
+INSERT INTO `artist` (`artist_id`, `artist_name`, `genre`, `overview`, `date_of_birth`)
+VALUES (1, 'Kendrick Lamar', 'Hip Hop', 'Kendrick Lamar is an American rapper, singer, songwriter, and record producer', '1987-02-16'),
+       (2, 'Jimi Hendrix', 'Rock', ' Hendrix was an American guitarist, singer, and songwriter', '1957-04-11'),
+       (3, 'Nas', 'Hip Hop', 'Nas, is an American rapper and entrepreneur.', '1975-02-16'),
+       (4, 'Taylor Swift', 'Pop', 'Taylor Swift is an American singer-songwriter.', '1989-02-16');
