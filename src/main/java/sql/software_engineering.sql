c77c9b59b173cea390bb1465cd2a3dc6566fec67
TobyZedomi <tobyzedo4@gmail.com>
2025-11-08 16:04:34 +0000

    First commit

---
+++ src/main/java/sql/software_engineering.sql
@@ -0,0 +1,45 @@
+DROP
+DATABASE IF EXISTS software_engineering;
+CREATE
+DATABASE IF NOT EXISTS software_engineering;
+
+USE
+software_engineering;
+
+CREATE TABLE users
+(
+    username    varchar(50) UNIQUE  NOT NULL,
+    password    varchar(255)        NOT NULL,
+    PRIMARY KEY (username)
+);
+
+CREATE TABLE artist
+(
+    artist_id        INT AUTO_INCREMENT,
+    artist_name      varchar(255) NOT NULL,
+    genre    varchar(255)         NOT NULL,
+    overview      varchar(255) NOT NULL,
+    date_of_birth DATE         NOT NULL,
+    PRIMARY KEY (artist_id)
+);
+
+
+CREATE TABLE album
+(
+    album_id        INT AUTO_INCREMENT,
+    artist_id      INT(11) NOT NULL,
+    description      varchar(255) NOT NULL,
+    date_of_release DATE         NOT NULL,
+    PRIMARY KEY (album_id),
+    FOREIGN KEY (artist_id) REFERENCES artist (artist_id)
+);
+
+CREATE TABLE review
+(
+    username VARCHAR(50) NOT NULL,
+    album_id INT NOT NULL,
+    rating double NOT NULL,
+    comment varchar(255) NOT NULL,
+    PRIMARY KEY (username, album_id),
+    FOREIGN KEY (username) REFERENCES users (username)
+)
